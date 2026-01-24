82d8b59fb5c488e6d60ba798189832c326e879dc
Sharva-k <kalesharva@gmail.com>
2026-01-24 21:09:34 +0530

    Minor changes

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         double sipResult = InvestmentPlanner.sip(5000,10,12);
         double lumpsumResult = InvestmentPlanner.lumpsum(100000,5, 10);
 
-        System.out.println("SIP Amount: ₹" + (int)sipResult);
+        System.out.println("SIP Amount: ₹ " + (int)sipResult);
         System.out.println("Lump Sum Amount: ₹" + (int)lumpsumResult);
     }
 }

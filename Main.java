c43befcac8c4266a8849a5564c905ba160472673
Sharva-k <kalesharva@gmail.com>
2026-01-24 21:13:51 +0530

    Minor changes

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    // Temporary code to test logical working
+    // Temporary code to test logical working , will make changes later
 
     public static void main(String[] args){
         double sipResult = InvestmentPlanner.sip(5000,10,12);

1efc21b8b38a001f823a6a60a285b112f956f565
Sharva-k <kalesharva@gmail.com>
2026-01-25 21:03:49 +0530

    Minor changes

--- ChartView.java
+++ ChartView.java
@@ -1,4 +1,4 @@
-// Planning to use JavaFX to show data mathematically (pie chart,line chart,comparison table,growth rate)
+// Planning to use JavaFX to show data mathematically (pie chart,line chart,comparison table,growth rate,ROI chart)
 
 public class ChartView {
     
